d4048394c6a796c05a94ced956fba7a6fc651dba
howard <howard.ueckert@protonmail.com>
2024-12-28 15:33:58 -0600

    add food Schema

--- models/user.js
+++ models/user.js
@@ -1,5 +1,20 @@
 const mongoose = require('mongoose');
 
+const foodSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  recipes: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    enum: [1, 2, 3, 4, 5],
+  },
+});
+
 const userSchema = mongoose.Schema({
   username: {
     type: String,
